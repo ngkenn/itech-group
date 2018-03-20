b54be58c9be80a77395c932085539297e3a4c352
parvir <parvirchomber@gmail.com>
2018-03-20 12:51:23 +0000

    User registration added

--- itechGroup/actifind/tests.py
+++ itechGroup/actifind/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from actifind.models import Review, Activity, Picture, UserProfile
 from django.contrib.staticfiles import finders
 from django.contrib.auth.models import User
-from actifind.forms import ReviewForm, UserForm, UserProfileForm, Activity
+from actifind.forms import UserForm, UserProfileForm, Activity
 from django.core.urlresolvers import reverse
 from django.contrib import auth
 from django.test.client import Client
@@ -13,28 +13,6 @@ class FindLogo(TestCase):
 
         result = finders.find('images/actifind_logo.png')
         self.assertIsNotNone(result)
-
-
-# class Login(TestCase):
-#     def test_for_logging_in(self):
-#         self.credentials = {'username' : 'testuser',
-#                             #'email': 'tester@gmail.com',
-#                             'password': 'secretpass'}
-#         User.objects.create_user(**self.credentials)
-#
-#     def test_login(self):
-#         self.credentials = {'username': 'testuser',
-#                             # 'email': 'tester@gmail.com',
-#                             'password': 'secretpass'}
-#
-#         user = auth.get_user(self.client)
-#
-#         self.assertTrue (user.is_authenticated)
-#
-#         response = self.client.post('register/', self.credentials, follow=True)
-#
-#         self.assertTrue(response.context['user'].is_active) #not working atm
-#
 
 
 class IndexTests(TestCase):
@@ -48,12 +26,14 @@ class IndexTests(TestCase):
 class ActivityTest(TestCase):
 
     def setUp(self):
-        Activity.objects.create(name="Photography", description="Great for when the sun is out")
-        Activity.objects.create(name="Rock Climbing",description="Good day out with the kids")
+        #Passing a user_id as the field User cannot be null
+        Activity.objects.create(name="Photography", description="Great for when the sun is out", user_id='4')
+
+        Activity.objects.create(name="Rock Climbing",description="Good day out with the kids", user_id='3')
 
 
     def test_activity_is_present(self):
-        #Test to see if inputted data is present
+        #Test to see if inputted data is present user needs a user ID
         photography = Activity.objects.get(name="Photography")
         rock_climbing = Activity.objects.get(name="Rock Climbing")
 
@@ -61,38 +41,15 @@ class ActivityTest(TestCase):
         self.assertEqual(rock_climbing.description, "Good day out with the kids")
 
 
-# class Registration(TestCase):
-#
-#     def test_authentication(self):
-#         response = self.client.post(reverse('index'), {'username': 'tester',
-#                                                        'email': 'tester@gmail.com',
-#                                                        'password': 'testerpass'})
-#
-#         user = auth.get_user(self.client)
-#
-#         c = Client()
-#         c.login(username='fred', password='secret')
-#
-#         #self.assertTrue(user.is_authenticated)
-#
-#         assert user.is_authenticated
+class LoginTest(TestCase):
 
-# class UserTest(TestCase):
-#
-#     def setUp(self):
-#         self.client = Client()
-
-
-class UserTEST(TestCase):
-    def test_create_user(self):
-        # Create a user
-        from actifind.models import UserProfile
-        user = UserProfile.objects.get_or_create(username="testuser", password="test1234",
-                                           email="testuser@testuser.com")[0]
-        user.set_password(user.password)
+    def setUp(self):
+        user = User.objects.create_user(username='tester')
+        user.set_password('er123')
         user.save()
 
-        return user
-
-        self.assertTrue(user.is_authenticated)
+    def test_log(self):
+        c = Client()
+        logged_in = c.login(username='tester', password='er123')
+        self.assertTrue(logged_in)
 
