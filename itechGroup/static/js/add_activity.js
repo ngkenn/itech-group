0b5ae4fad269c1c096ae45ae9609a82f63fdf119
jgalvan <galvan.vil@gmail.com>
2018-03-20 13:34:06 +0000

    Fix jquery hash and separate js from add_activity view

---
+++ itechGroup/static/js/add_activity.js
@@ -0,0 +1,21 @@
+var autocomplete;
+
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+                    /** @type {!HTMLInputElement} */(document.getElementById('id_address')),
+        { types: ['geocode'] });
+
+    // When the user selects an address from the dropdown, populate the address
+    // fields in the form.
+    autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    var place = autocomplete.getPlace();
+    frame_maps = document.getElementById("iframemaps");
+    frame_maps.setAttribute("src", "https://www.google.com/maps/embed/v1/place?key=" + GOOGLE_MAPS_KEY +"&q=" + place.formatted_address);
+    frame_maps.removeAttribute("hidden");
+}
